c4393ae13b77ffd41b1ed189d8fe04f95a9c81ac
Mohamed Belhassen <mohamed.belhassen@gmail.com>
2022-05-19 13:41:59 +0100

    working project :)

--- constants/MyKeys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  applicationId: 'Z3MpGaCLHDPUjGPyBeYzD0NNx3rvY7kfTBF1p3Ji',
-  javascriptKey: 'ehQSn6T2GtDNTyPduLQP6LtPVeas7TExTNR7djN8',
-  serverURL: 'https://parseapi.back4app.com/',
-};
